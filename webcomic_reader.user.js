c46ae14a02375ed2bb35894ead6e3667e62c7e10
SoraHjort <callmehjort@gmail.com>
2019-10-05 20:38:01 -0800

    Update webcomic_reader.user.js
    And the +...

--- webcomic_reader.user.js
+++ webcomic_reader.user.js
@@ -6030,7 +6030,7 @@ function ifMobile(){
                 'color: #222;'+
                 'background-color: #ccc !important;'+
                 'border: 2px solid rgba(22,22,22,0.3);'+
-                'font-family: "Lucida Grande", sans-serif !important;'
+                'font-family: "Lucida Grande", sans-serif !important;'+
                 'display: block;'+
                 'margin: 20px auto auto auto;'+
                 'width: 50%;'+
