bba53d8fa0cd04018c7588d2961fac78f9f0540e
Sora Hjort <callmehjort@gmail.com>
2022-02-07 04:45:43 -0900

    Added: Wukrii
    https://www.wukrii.com/

--- webcomic_reader.user.js
+++ webcomic_reader.user.js
@@ -43,7 +43,7 @@ var defaultSettings = {
 // ==UserScript==
 // @name		Webcomic Reader - Sora Testing
 // @author		Javier Lopez <ameboide@gmail.com> https://github.com/ameboide , fork by v4Lo https://github.com/v4Lo and by anka-213 http://github.com/anka-213
-// @version		2022.02.07.013500
+// @version		2022.02.07.044000
 // @license		MIT
 // @namespace	http://userscripts.org/scripts/show/59842
 // @description	Can work on almost any webcomic/manga page, preloads 5 or more pages ahead (or behind), navigates via ajax for instant-page-change, lets you use the keyboard, remembers your progress, and it's relatively easy to add new sites
@@ -591,6 +591,7 @@ var defaultSettings = {
 // @include     http*://alphaluna.net/*
 // @include     http*://themonsterunderthebed.net/*
 // @include     http*://www.starwarriorscomic.com/*
+// @include     http*://www.wukrii.com/*
 
 // ==/UserScript==
 
@@ -3139,6 +3140,9 @@ var paginas = [
 		img:	['(//div[@id="comic"]/a/img|//div[@id="comic"]/img)[last()]'],
         extra:  [['//div[@id="content"]']],
         layelem:'//div[@id="page-wrap"]',
+	},
+	{	url:	'wukrii.com',
+		img:	['(//div[@id="comic"]/img|//div[@id="comic"]/a/img)'],
 	}
 	// End of sites
 	/*
